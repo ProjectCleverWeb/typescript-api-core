cd731a3b67497db0537c1a771f698f8c6b1dc9c3
Nicholas Summers <projectcleverweb@gmail.com>
2020-11-07 18:21:26 -0500

    feat: update serverless config

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ module.exports.application = async event => {
 		statusCode : 200,
 		body       : JSON.stringify(
 			{
-				message : 'Go Serverless v1.0! Your function executed successfully!',
+				message : 'Hello World!',
 				input   : event,
 			},
 			null,
