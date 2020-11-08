b89cdfca2f2771a251f3a2891d0f0e186a37c1a6
Nicholas Summers <projectcleverweb@gmail.com>
2020-11-08 14:24:20 -0500

    feat: restructure api entry point

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,7 @@
 import 'source-map-support/register'
 
 // Bootstrap the environment and api core
-import { api } from './core/bootstrap'
+import { application } from './core/bootstrap'
 
 // "and the monkey flips the switch" (Launch!)
-const application = async (event: object, context: object) => {
-  const output = {
-		statusCode : 200,
-		body       : JSON.stringify(
-			{
-				message : await api.test(event, context)
-			},
-			null,
-			2,
-		),
-	}
-	console.log("Request Output: \n" + output.body)
-	return output
-}
-
-
-export default application
+export default application.api
--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -1,6 +1,19 @@
 
-export class api {
-	public static async test(event: object, context: object) {
-		return 'Hello from TypeScript!'
+export class application {
+	public static async api(event: object, context: object) {
+		const output = {
+			statusCode : 200,
+			body       : JSON.stringify(
+				{
+					message : 'Hello from TypeScript!'
+				},
+				null,
+				2,
+			),
+		}
+		console.log("Request Output: \n" + output.body)
+		return output
 	}
+	
 }
+
