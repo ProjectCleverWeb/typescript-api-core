01c51f43ca55c57462a9836fde7d20d3d3221c4f
Nicholas Summers <projectcleverweb@gmail.com>
2020-11-08 21:20:07 -0500

    feat: create output class

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -1,18 +1,23 @@
+import {output} from "./class/output"
 
 export class application {
 	public static async api(event: object, context: object) {
-		const output = {
-			statusCode : 200,
-			body       : JSON.stringify(
-				{
-					message : 'Hello from TypeScript!'
-				},
-				null,
-				2,
-			),
-		}
-		console.log("Request Output: \n" + output.body)
-		return output
+		
+		await output.metaReplace('message', 'Hello from TypeScript!')
+		await output.append('test', [
+			{
+				a: "foo",
+				b: "bar",
+				c: "baz",
+			}
+		])
+		await output.append('test', [
+			{
+				hello: "world"
+			}
+		])
+		
+		return await output.render()
 	}
 	
 }
---
+++ src/core/class/output.ts
@@ -0,0 +1,62 @@
+
+
+export type T_outputBodyValue = [{[key: string] : any}]
+
+export type T_outputBody = {
+	[key: string] : T_outputBodyValue
+}
+
+export type T_outputMetaValue = string | number | Array<T_outputMetaValue> | {[key: string] : T_outputMetaValue}
+
+export type T_outputMeta = {
+	[key: string] : T_outputMetaValue
+}
+
+export class output {
+	
+	public static body: T_outputBody = {}
+	public static meta: T_outputMeta = {}
+	
+	public static async render() {
+		const output = {
+			statusCode : 200,
+			body       : JSON.stringify(
+				{
+					meta : this.meta,
+					body : this.body,
+				},
+				null,
+				2,
+			),
+		}
+		console.log("Request Output: \n" + output.body)
+		return output
+	}
+	
+	public static async replace(key: string, value: T_outputBodyValue) {
+		this.body[key] = value
+		return value
+	}
+	
+	public static async metaReplace(key: string, value: T_outputMetaValue) {
+		this.meta[key] = value
+		return value
+	}
+	
+	public static async append(key: string, value: T_outputBodyValue) {
+		if (await this.exists(key)) {
+			this.body[key].push(...value)
+		} else {
+			await this.replace(key, value)
+		}
+		return value
+	}
+	
+	public static async exists(key: string) {
+		return typeof this.body[key] !== 'undefined'
+	}
+	
+	
+}
+
+
