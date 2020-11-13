58227da67ab8fba4914163b0f5cbc5afa60eb113
Nicholas Summers <projectcleverweb@gmail.com>
2020-11-13 01:26:52 -0500

    feat: WIP on detect class

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -1,21 +1,42 @@
 import {output} from "./class/output"
+import {detect} from "./class/detect"
+
 
 export class application {
 	public static async api(event: object, context: object) {
-		
 		await output.metaReplace('message', 'Hello from TypeScript!')
-		await output.append('test', [
+		
+		await output.append('type_test', [
 			{
-				a: "foo",
-				b: "bar",
-				c: "baz",
+				test1: await detect.isNumber(1, false),
+				test2: await detect.isNumber("010110", false),
+				test3: await detect.isNumber("010110sxxxd", false),
+				test4: parseInt("010110sxxxd"),
 			}
 		])
-		await output.append('test', [
-			{
-				hello: "world"
-			}
-		])
+		
+		// await output.append('type_test', [
+		// 	{
+		// 		'null detected as': await detect.type(null),
+		// 		'[] detected as': await detect.type([]),
+		// 		'Number.NaN detected as': await detect.type(Number.NaN),
+		// 		'Number.POSITIVE_INFINITY detected as': await detect.type(Number.POSITIVE_INFINITY),
+		// 		'Number.NEGATIVE_INFINITY detected as': await detect.type(Number.NEGATIVE_INFINITY),
+		// 		'"" detected as': await detect.type(""),
+		// 		'"0" detected as': await detect.type("0"),
+		// 		'0 detected as': await detect.type(0),
+		// 		'1 detected as': await detect.type(1),
+		// 		'BigInt(9007199254740991) detected as': await detect.type(BigInt(9007199254740991)),
+		// 		'BigInt(1) detected as': await detect.type(BigInt(1)),
+		// 		'false detected as': await detect.type(false),
+		// 		'true detected as': await detect.type(true),
+		// 		'Symbol("foo") detected as': await detect.type(Symbol("foo")),
+		// 		'undefined detected as': await detect.type(undefined),
+		// 		'{} detected as': await detect.type({}),
+		// 		'() => {} detected as': await detect.type(() => {}),
+		// 		'detect.type detected as': await detect.type(detect.type),
+		// 	}
+		// ])
 		
 		return await output.render()
 	}
--- src/core/class/detect.ts
+++ src/core/class/detect.ts
@@ -1,40 +1,73 @@
 
-export type E_detectType = "null" | "array" | "nan" | "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"
+export type T_detectType = "null" | "array" | "nan" | "infinity" | "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"
+
+export enum E_detectNumber {
+	number,
+	bigint,
+	string,
+}
 
 export class detect {
-	public static async type(value: any) : Promise<E_detectType> {
+	
+	/**
+	 * Detect the type of a variable. This is an extension of typeof which adds
+	 * support for distinguishing the following types:
+	 * - null
+	 * - array
+	 * - nan
+	 * - infinity
+	 *
+	 * @param value
+	 * @return {Promise<T_detectType>} The type of the value
+	 */
+	public static async type(value: any) : Promise<T_detectType> {
 		const type   = typeof value
 		const object = value instanceof Object
 		
 		if (value === null) {
 			return 'null'
-		} else if (Number.isNaN(value)) {
+		} else if (type === 'number' && Number.isNaN(value)) {
 			return 'nan'
+		} else if (type === 'number' && !Number.isFinite(value)) {
+			return 'infinity'
 		} else if ((object || type === 'object') && value.constructor === Array) {
 			return 'array'
 		}
 		return type
 	}
 	
+	public static async isType(value: any, type: T_detectType) : Promise<boolean> {
+		return await this.type(value) === type
+	}
+	
 	public static async isNumber(value: any, strict: boolean = true) : Promise<boolean> {
+		const type = await this.type(value)
 		if (strict) {
-			return await this.type(value) === 'number'
+			return type === 'number'
+		} else if (type in E_detectNumber) {
+			return !Number.isNaN(Number.parseFloat(value))
 		}
-		return Number.isNaN(Number.parseFloat(value))
+		return false
 	}
 	
 	public static async isInt(value: any, strict: boolean = true) : Promise<boolean> {
+		const type = await this.type(value)
 		if (strict) {
-			return Number.isInteger(value)
+			return type === 'number' && Number.isInteger(value)
+		} else if (type in E_detectNumber) {
+			return Number.isInteger(Number.parseFloat(value))
 		}
-		return Number.isInteger(Number.parseFloat(value))
+		return false
 	}
 	
 	public static async isFloat(value: any, strict: boolean = true) : Promise<boolean> {
+		const type = await this.type(value)
 		if (strict) {
-			return await this.isNumber(value, true) && !await this.isInt(value, true)
+			return type === 'number' &&  !await this.isInt(value, strict)
+		} else if (type in E_detectNumber) {
+			return !await this.isInt(Number.parseFloat(value), strict)
 		}
-		return !await this.isInt(Number.parseFloat(value), false)
+		return false
 	}
 	
 	public static async isFinite(value: any, strict: boolean = true) : Promise<boolean> {
@@ -53,6 +86,39 @@ export class detect {
 	
 	public static async isNegative(value: any, strict: boolean = true) : Promise<boolean> {
 		return await this.isNumber(value, strict) && value < 0
+	}
+	
+	public static async isBinaryString(value: any) : Promise<boolean> {
+		return await this.type(value) === 'string' && /\A[01]\Z/.test(value);
+	}
+	
+	public static async isBinaryArray(value: any, strict: boolean = true) : Promise<boolean> {
+		if (await this.type(value) === 'array') {
+			// This functionally will only be boolean, the promise is to take care of TS warnings
+			let valid : boolean|Promise<boolean> = true
+			value.map((element: any) => {
+				valid = (async () => {return await this.isBoolean(element, strict)})()
+			})
+			return valid
+		}
+		return false
+	}
+	
+	public static async isBoolean(value: any, strict: boolean = true) : Promise<boolean> {
+		const type = await this.type(value)
+		if (strict || type === 'boolean') {
+			return type === 'boolean'
+		}
+		if (await this.isInt(value, strict)) {
+			value = parseInt(value)
+			console.log(value)
+			switch (value) {
+				case 0:
+				case 1:
+					return true
+			}
+		}
+		return false
 	}
 	
 }
