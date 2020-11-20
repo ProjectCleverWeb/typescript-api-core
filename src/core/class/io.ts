08afde360aa1c67d84d6048d74870570d73add95
Nicholas Summers <projectcleverweb@gmail.com>
2020-11-20 01:15:57 -0500

    feat: start work on IO class

---
+++ src/core/class/io.ts
@@ -0,0 +1,19 @@
+
+const bitwise = {
+	bit1: 1 << 0,
+	bit2: 1 << 1,
+	bit3: 1 << 2,
+	bit4: 1 << 3,
+}
+
+export enum E_ioTypes {
+	phone = bitwise.bit1
+}
+
+export class io {
+	public static async create(key: string, type: E_ioTypes) {
+		return 1 + type
+	}
+}
+
+
